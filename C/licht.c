3ccec968b7df83987fec4825329033ad4b4f34f5
annedeg <a.d.graaff@hotmail.com>
2019-11-04 12:47:05 +0100

    Fixed things

--- C/licht.c
+++ C/licht.c
@@ -1,35 +1,29 @@
-#include <asf.h>
 #include <avr/io.h>
+volatile int light_off = 0;
+void turn_light_off() {
+	light_off = 1;
+}
 
-volatile uint8_t lightvalue;
-
-int main (void)
+void init_adc()
 {
-	void init_adc()
-	{
-		// ref=Vcc, left adjust the result (8 bit resolution),
-		// select channel 1 (PC1 = input)
-		DDRB = 0xff;
-		ADMUX = MUX1|(1<<REFS0)|(1<<ADLAR);
-		// enable the ADC & prescale = 128
-		ADCSRA = (1<<ADEN)|(1<<ADPS2)|(1<<ADPS1)|(1<<ADPS0);
-	}
-	uint8_t get_adc_value()
-	{
+	// ref=Vcc, left adjust the result (8 bit resolution),
+	// select channel 1 (PC1 = input)
+	ADMUX = 0x00;
+	ADMUX = MUX1|(1<<REFS0)|(1<<ADLAR);
+	// enable the ADC & prescale = 128
+	ADCSRA = (1<<ADEN)|(1<<ADPS2)|(1<<ADPS1)|(1<<ADPS0);
+}
+
+int get_adc_value()
+{
+	if(light_off != 1) {
+		init_adc();
 		ADCSRA |= (1<<ADSC); // start conversion
 		loop_until_bit_is_clear(ADCSRA, ADSC);
-		return ADCH; // 8-bit resolution, left adjusted
+		int light_value = (int) ADCH;
+		show_distance(light_value);
+		return light_value; // 8-bit resolution, left adjusted
 	}
-	void loop()
-	{
-		lightvalue = get_adc_value(); //put value in variable
-		if(lightvalue > 0x7f)				//this if statement will limit it to 7 bits
-		{
-			lightvalue = 0x7f;					//if the value is more than 7 bits can handle it will make in maximum of 7 bits
-		}
-	}
-	init_adc();								//these statements let the program run.
-	while(1){
-		loop();
-	}
+	return 0;
 }
+
